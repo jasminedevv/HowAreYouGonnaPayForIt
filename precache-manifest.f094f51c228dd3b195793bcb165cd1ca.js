3e5905c9adf2ef60a56a656f885aef1cd883dd94
Jasmine Humbert <jasmine.yhumbert@gmail.com>
2019-03-21 15:18:43 -0700

    Updates

---
+++ precache-manifest.f094f51c228dd3b195793bcb165cd1ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HowAreYouGonnaPayForIt/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "606b9841a4a438e6e022",
+    "url": "/HowAreYouGonnaPayForIt/static/js/runtime~main.2ac2bc13.js"
+  },
+  {
+    "revision": "6491407d167b4507ea97",
+    "url": "/HowAreYouGonnaPayForIt/static/js/main.56a2de7b.chunk.js"
+  },
+  {
+    "revision": "6356e81d9662880b29f1",
+    "url": "/HowAreYouGonnaPayForIt/static/js/2.47dd1590.chunk.js"
+  },
+  {
+    "revision": "6491407d167b4507ea97",
+    "url": "/HowAreYouGonnaPayForIt/static/css/main.1d5779a1.chunk.css"
+  },
+  {
+    "revision": "c68cf3d4ddfab67146f7e3414a13b01e",
+    "url": "/HowAreYouGonnaPayForIt/index.html"
+  }
+];
